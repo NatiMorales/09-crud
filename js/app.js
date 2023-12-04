4726388e3c4e28a68cf77e96df0bc2d24a1ae305
Natalia Morales <natii.vmorales98@gmail.com>
2023-12-04 09:27:48 -0300

    practicamos con los tipos de export

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import {Contacto,prueba} from "./classContacto.js";
+
+const contactoNuevo = new Contacto(1, "algun nombre", "apellido", "skjdkfj@jf.com", 734789);
+console.log(contactoNuevo);
+
+console.log(prueba);
